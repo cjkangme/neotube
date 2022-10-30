2eadb00bb8658f3bb916a45b18bd37e9ffcd594d
cjkangme <nureongi0214@gmail.com>
2022-10-30 19:33:14 +0900

    [#5.0 ~ #5.4] Feat: Add pug

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ function handleListening() {
   console.log(`Server On (Port : ${PORT})`);
 }
 
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use('/', globalRouter);
 app.use('/users', userRouter);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,9 +9,9 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get('/:id', watchVideo);
-videoRouter.get('/:id/edit', editVideo);
-videoRouter.get('/:id/delete', deleteVideo);
 videoRouter.get('/upload', uploadVideo);
+videoRouter.get('/:id(\\d+)', watchVideo);
+videoRouter.get('/:id(\\d+)/edit', editVideo);
+videoRouter.get('/:id(\\d+)/delete', deleteVideo);
 
 export default videoRouter;
