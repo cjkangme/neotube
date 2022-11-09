4b58351e1d9eab7ba4620f39a1dd43e2dbd87c81
cjkangme <nureongi0214@gmail.com>
2022-11-09 14:52:31 +0900

    #7.15 Environment Variables
    - dotenv package install
    - 환경 변수 설정

--- src/server.js
+++ src/server.js
@@ -18,11 +18,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.DB_URL,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/neotube",
+      mongoUrl: process.env.DB_URL,
     }),
   })
 );
