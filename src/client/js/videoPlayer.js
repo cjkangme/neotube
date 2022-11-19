b2bdc5ca591b9e9d71915951f03813639a25559b
cjkangme <nureongi0214@gmail.com>
2022-11-19 17:31:19 +0900

    #11.4 Duration and Current TIme
    - 현재 시간 및 전체 시간 표시기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,15 +1,35 @@
 const video = document.querySelector("video");
 const playBtn = document.querySelector("#play");
 const muteBtn = document.querySelector("#mute");
-const time = document.querySelector("#time");
+const currentTimeSpan = document.querySelector("#current-time");
+const totalTimeSpan = document.querySelector("#total-time");
 const volumeInput = document.querySelector("#volume");
 
 let volume = 0.5;
 video.volume = volume;
 
-console.log(playBtn, mute, time, volume);
+console.log(playBtn);
 
-const handleMuteClick = (event) => {
+// sub function
+
+const formatTime = (time) => {
+  const min = String(Math.floor(time / 60)).padStart(2, "0");
+  const sec = String(Math.floor(time % 60)).padStart(2, "0");
+  return `${min}:${sec}`;
+};
+
+// event function
+const handleLoadedMetadata = () => {
+  const { duration } = video;
+  totalTimeSpan.innerText = formatTime(duration);
+};
+
+const handleTimeUpdate = () => {
+  const { currentTime } = video;
+  currentTimeSpan.innerText = formatTime(currentTime);
+};
+
+const handleMuteClick = () => {
   if (video.muted) {
     video.muted = false;
     volumeInput.value = video.volume;
@@ -45,9 +65,13 @@ const handleVolumeInput = (event) => {
   video.volume = value;
 };
 
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+
 muteBtn.addEventListener("click", handleMuteClick);
+video.addEventListener("volumechange", handleVolumeChange);
+volumeInput.addEventListener("input", handleVolumeInput);
+
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
 playBtn.addEventListener("click", handlePlayClick);
-video.addEventListener("volumechange", handleVolumeChange);
-volumeInput.addEventListener("input", handleVolumeInput);
