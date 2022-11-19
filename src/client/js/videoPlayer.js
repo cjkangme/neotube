bb9a9c8a29e3c59754b339577dd24c384d9111aa
cjkangme <nureongi0214@gmail.com>
2022-11-19 17:11:43 +0900

    #11.3 Volume
    - 비디오 볼륨 조절기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,11 +2,22 @@ const video = document.querySelector("video");
 const playBtn = document.querySelector("#play");
 const muteBtn = document.querySelector("#mute");
 const time = document.querySelector("#time");
-const volume = document.querySelector("#volume");
+const volumeInput = document.querySelector("#volume");
+
+let volume = 0.5;
+video.volume = volume;
 
 console.log(playBtn, mute, time, volume);
 
-const handleMuteClick = (event) => {};
+const handleMuteClick = (event) => {
+  if (video.muted) {
+    video.muted = false;
+    volumeInput.value = video.volume;
+  } else {
+    video.muted = true;
+    volumeInput.value = 0;
+  }
+};
 
 const handlePause = () => {
   playBtn.innerText = "Play";
@@ -24,7 +35,19 @@ const handlePlayClick = (event) => {
   }
 };
 
+const handleVolumeChange = () => {
+  muteBtn.innerText =
+    video.muted || volumeInput.value < 0.02 ? "Unmute" : "Mute";
+};
+
+const handleVolumeInput = (event) => {
+  const value = event.target.value;
+  video.volume = value;
+};
+
 muteBtn.addEventListener("click", handleMuteClick);
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
 playBtn.addEventListener("click", handlePlayClick);
+video.addEventListener("volumechange", handleVolumeChange);
+volumeInput.addEventListener("input", handleVolumeInput);
