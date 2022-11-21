408f5057c047fc6275f445438d6d992bb7095fc5
cjkangme <nureongi0214@gmail.com>
2022-11-21 13:15:51 +0900

    #11.7 Timeline & Fullscreen
    - Timeline 및 동영상 시간 조절 추가
    - Fullscreen mode ON/OFF 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,32 +1,42 @@
 const video = document.querySelector("video");
+const videoContainer = document.querySelector("#video-container");
 const playBtn = document.querySelector("#play");
 const muteBtn = document.querySelector("#mute");
 const currentTimeSpan = document.querySelector("#current-time");
 const totalTimeSpan = document.querySelector("#total-time");
 const volumeInput = document.querySelector("#volume");
+const timeline = document.querySelector("#timeline");
+const fullscreenBtn = document.querySelector("#fullscreen");
 
 let volume = 0.5;
 video.volume = volume;
 
-console.log(playBtn);
-
 // sub function
 
 const formatTime = (time) => {
-  const min = String(Math.floor(time / 60)).padStart(2, "0");
-  const sec = String(Math.floor(time % 60)).padStart(2, "0");
-  return `${min}:${sec}`;
+  let timeString = new Date(time * 1000).toISOString().substring(11, 19);
+  if (timeString.substring(0, 2) === "00") {
+    return timeString.substring(3);
+  } else {
+    return timeString;
+  }
 };
 
 // event function
 const handleLoadedMetadata = () => {
   const { duration } = video;
   totalTimeSpan.innerText = formatTime(duration);
+  timeline.max = Math.floor(duration);
 };
 
 const handleTimeUpdate = () => {
   const { currentTime } = video;
   currentTimeSpan.innerText = formatTime(currentTime);
+  timeline.value = Math.floor(currentTime);
+};
+
+const handleTimeline = (event) => {
+  video.currentTime = event.target.value;
 };
 
 const handleMuteClick = () => {
@@ -65,8 +75,25 @@ const handleVolumeInput = (event) => {
   video.volume = value;
 };
 
+const handleFullscreen = () => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    videoContainer.requestFullscreen();
+  }
+};
+
+const handleFullscreenChange = () => {
+  if (document.fullscreenElement) {
+    fullscreenBtn.innerText = "Exit";
+  } else {
+    fullscreenBtn.innerText = "Fullscreen";
+  }
+};
+
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimeline);
 
 muteBtn.addEventListener("click", handleMuteClick);
 video.addEventListener("volumechange", handleVolumeChange);
@@ -75,3 +102,6 @@ volumeInput.addEventListener("input", handleVolumeInput);
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
 playBtn.addEventListener("click", handlePlayClick);
+
+fullscreenBtn.addEventListener("click", handleFullscreen);
+videoContainer.addEventListener("fullscreenchange", handleFullscreenChange);
