04940e75ecd90d071e2cdb28fbae8ce162a38891
cjkangme <nureongi0214@gmail.com>
2022-12-04 23:52:47 +0900

    FIX: comment의 time format이 되지 않는 문제

--- src/client/js/timeFormat.js
+++ src/client/js/timeFormat.js
@@ -1,54 +1,57 @@
 const dateStr = document.querySelectorAll(".video-mixin__createdAt");
+const commentStr = document.querySelectorAll(".comment__created");
 const videoDate = document.querySelector(".video__description-createdAt");
 const userDate = document.querySelector(".profile__user-createdAt");
 
 // mixin에서 보이는 업로드 날짜 format
-dateStr.forEach((item) => {
-  const date = new Date(item.innerText);
-  const now = Date.now();
-  const dateNum = date.getTime(date);
-  const relativeTime = dateNum - now;
-  const relativeMin = parseInt(relativeTime / (1000 * 60));
-  const relativeHour = parseInt(relativeMin / 60);
-  const relativeDay = parseInt(relativeHour / 24);
-  const relativeWeek = parseInt(relativeDay / 7);
-  const relativeMonth = parseInt(relativeDay / 30);
-  const relativeYear = parseInt(relativeDay / 365);
-  let text;
-  let formatNum;
-  let type;
+const rtfTime = (items) => {
+  items.forEach((item) => {
+    const date = new Date(item.innerText);
+    const now = Date.now();
+    const dateNum = date.getTime(date);
+    const relativeTime = dateNum - now;
+    const relativeMin = parseInt(relativeTime / (1000 * 60));
+    const relativeHour = parseInt(relativeMin / 60);
+    const relativeDay = parseInt(relativeHour / 24);
+    const relativeWeek = parseInt(relativeDay / 7);
+    const relativeMonth = parseInt(relativeDay / 30);
+    const relativeYear = parseInt(relativeDay / 365);
+    let text;
+    let formatNum;
+    let type;
 
-  const rtf = new Intl.RelativeTimeFormat("ko-KR");
+    const rtf = new Intl.RelativeTimeFormat("ko-KR");
 
-  if (relativeYear <= -1) {
-    formatNum = relativeYear;
-    type = "year";
-  } else if (relativeMonth <= -1) {
-    formatNum = relativeMonth;
-    type = "month";
-  } else if (relativeWeek <= -1) {
-    formatNum = relativeWeek;
-    type = "week";
-  } else if (relativeDay <= -1) {
-    formatNum = relativeDay;
-    type = "day";
-  } else if (relativeHour <= -1) {
-    formatNum = relativeHour;
-    type = "hour";
-  } else if (relativeMin <= -1) {
-    formatNum = relativeMin;
-    type = "minute";
-  } else {
-    formatNum = 0;
-  }
+    if (relativeYear <= -1) {
+      formatNum = relativeYear;
+      type = "year";
+    } else if (relativeMonth <= -1) {
+      formatNum = relativeMonth;
+      type = "month";
+    } else if (relativeWeek <= -1) {
+      formatNum = relativeWeek;
+      type = "week";
+    } else if (relativeDay <= -1) {
+      formatNum = relativeDay;
+      type = "day";
+    } else if (relativeHour <= -1) {
+      formatNum = relativeHour;
+      type = "hour";
+    } else if (relativeMin <= -1) {
+      formatNum = relativeMin;
+      type = "minute";
+    } else {
+      formatNum = 0;
+    }
 
-  if (formatNum === 0) {
-    text = "방금 전";
-  } else {
-    text = rtf.format(formatNum, type);
-  }
-  item.innerText = text;
-});
+    if (formatNum === 0) {
+      text = "방금 전";
+    } else {
+      text = rtf.format(formatNum, type);
+    }
+    item.innerText = text;
+  });
+};
 
 const dtfTime = (item, text) => {
   if (item) {
@@ -60,5 +63,7 @@ const dtfTime = (item, text) => {
 };
 
 // watch 화면에서 보이는 업로드 날짜 포맷
+rtfTime(dateStr);
+rtfTime(commentStr);
 dtfTime(videoDate, "업로드: ");
 dtfTime(userDate, "");
